4c6d1a2cf437c59c4ab6dd9e121e6a9cddd3ec2a
furox94@gmail.com <furox94@gmail.com@01102ac3-07b9-44c6-6fa8-949ad3c80621>
2013-02-22 20:49:44 +0000

    Update 0.9.2

--- Launcher/AionLauncher/Program.cs
+++ Launcher/AionLauncher/Program.cs
@@ -12,12 +12,22 @@ namespace AionLauncher
         [STAThread]
         static void Main()
         {
-            //check to see if the DLL dependency is missing -- HtmlRendered.dll
+            //check to see if one of the DLL dependency is missing -- HtmlRendered.dll
             if (!System.IO.File.Exists("HtmlRenderer.dll"))
             {
                 MessageBox.Show("You must copy HtmlRenderer.dll into your aion folder.", "Error loading DLL", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             } //end if
+            if (!System.IO.File.Exists("Ionic.Zip.dll"))
+            {
+                MessageBox.Show("You must copy Ionic.Zip.dll into your aion folder.", "Error loading DLL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!System.IO.File.Exists("Nini.dll"))
+            {
+                MessageBox.Show("You must copy Ionic.Zip.dll into your aion folder.", "Error loading DLL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
