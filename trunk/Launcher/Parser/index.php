420184167f08ef9cac99c0af679ea75d769f8366
furox94@gmail.com <furox94@gmail.com@01102ac3-07b9-44c6-6fa8-949ad3c80621>
2013-06-21 18:06:03 +0000

    changelog

--- trunk/Launcher/Parser/index.php
+++ trunk/Launcher/Parser/index.php
@@ -1,4 +1,6 @@
 {
 "version": "1.0.0.0",
-"download": "https://code.google.com/p/aion-launcher-beta/downloads/detail?name=Aion%20Launcher%201.0.exe"
+"download": "https://code.google.com/p/aion-launcher-beta/downloads/detail?name=Aion%20Launcher%201.0.exe",
+"gameclient": "3.0.0.8",
+"changelog": "What's New:\r\n\r\n• Support multi languages\r\n• Fix some bugs"
 }
