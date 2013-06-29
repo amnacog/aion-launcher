0aad41d42841059f602dce55166990746e41093b
furox94@gmail.com <furox94@gmail.com@01102ac3-07b9-44c6-6fa8-949ad3c80621>
2013-06-29 12:41:37 +0000

    Update parser 1.2

--- Launcher/Parser/index.php
+++ Launcher/Parser/index.php
@@ -1,6 +1,6 @@
 {
-"version": "1.1",
-"download": "http://code.google.com/p/aion-launcher-beta/downloads/detail?name=AionLauncher-1.1%20Setup.exe",
+"version": "1.2",
+"download": "http://code.google.com/p/aion-launcher-beta/downloads/detail?name=AionLauncher-1.2%20Setup.exe",
 "gameclient": "3.0.0.8",
-"changelog": "What's New:\r\n\r\n• Support multi languages\r\n• Fix some bugs (ui,controls)"
+"changelog": "What's New:\r\n• Program space significantly reduced\r\n\r\n• Automatic downloader for future updates\r\n• Optimization code (ui,controls)"
 }
