30a2d2b743830589a14af633c9c7227399be1fad
furox94@gmail.com <furox94@gmail.com@01102ac3-07b9-44c6-6fa8-949ad3c80621>
2013-07-30 12:58:34 +0000

    parser

---
+++ Parser/parser1.php
@@ -0,0 +1,6 @@
+{
+"version": "1.3",
+"download": "https://aion-launcher-beta.googlecode.com/files/AionLauncher-1.2%20Setup.exe",
+"gameclient": "3.0.0.8",
+"changelog": "What's New:\r\n\r\n• New verification method to update (md5)\r\n• Optmization for updater\r\n• Debug some code.."
+}
