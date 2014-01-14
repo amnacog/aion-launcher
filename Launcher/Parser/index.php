cf40fb8f571e9cfca4cefd146e926ebdc98be277
furox94@gmail.com <furox94@gmail.com@01102ac3-07b9-44c6-6fa8-949ad3c80621>
2014-01-14 00:49:14 +0000

    Update parser to 1.4 version of the launcher

--- Launcher/Parser/index.php
+++ Launcher/Parser/index.php
@@ -1,5 +1,5 @@
 {
-"version": "1.3",
+"version": "1.4",
 "download": "https://aion-launcher-beta.googlecode.com/files/AionLauncher-1.3%20Setup.exe",
 "gameclient": "3.0.0.8",
 "changelog": "What's New:\r\n\r\n• New verification method to update (md5)\r\n• Multi game version patcher\r\n• Can now disable the game checker (manually)\r\n• Optmization code for the updater\r\n• Debug some code.."
