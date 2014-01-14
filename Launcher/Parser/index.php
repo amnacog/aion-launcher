0b6643c673439ba72394e6a101ad15619b0b5618
furox94@gmail.com <furox94@gmail.com@01102ac3-07b9-44c6-6fa8-949ad3c80621>
2014-01-14 00:55:15 +0000

    Update update message

--- Launcher/Parser/index.php
+++ Launcher/Parser/index.php
@@ -2,5 +2,5 @@
 "version": "1.4",
 "download": "https://aion-launcher-beta.googlecode.com/files/AionLauncher-1.3%20Setup.exe",
 "gameclient": "3.0.0.8",
-"changelog": "What's New:\r\n\r\n• New verification method to update (md5)\r\n• Multi game version patcher\r\n• Can now disable the game checker (manually)\r\n• Optmization code for the updater\r\n• Debug some code.."
+"changelog": "What's New:\r\n\r\n• Minor update & debug some code\r\n• The game checker is now disable by default\r\n(can be re-enabled by selecting another version of game)\r\n• Last Upload from google code :(\r\n• Happy New year :)"
 }
